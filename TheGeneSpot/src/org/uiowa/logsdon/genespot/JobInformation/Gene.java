c3659aba447c2efbb7983b58c3b708916a4dd408
Austin Ward <austin-ward@uiowa.edu>
2017-02-12 14:40:40 -0600

    Updated - need to fix XML parser

---
+++ TheGeneSpot/src/org/uiowa/logsdon/genespot/JobInformation/Gene.java
@@ -0,0 +1,32 @@
+package JobInformation;
+/*
+
+ *
+ * Created on Dec 2, 2016
+ * Author: austinward 
+ *
+ */
+
+public class Gene {
+
+	private final String geneName;
+	private final ProteinQuery[] proteinQueries;
+
+
+	public Gene(String geneName, ProteinQuery[] proteinQueries) {
+
+		this.geneName = geneName;
+		this.proteinQueries = proteinQueries;
+
+	}
+
+	public String getName() {
+
+		return geneName;
+	}
+
+	public ProteinQuery[] getProteinQueries(){
+
+		return this.proteinQueries;
+	}
+}
---
+++ TheGeneSpot/src/org/uiowa/logsdon/genespot/JobInformation/mainTest.java
@@ -0,0 +1,67 @@
+package JobInformation;
+
+
+
+import NCBI.MakeRequest;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ * Created on Nov 30, 2016
+ * Author: austinward 
+ *
+ */
+public class mainTest {
+
+	public static void main(String[] argv) {
+
+		String jobName = "Testing";
+
+		double evalue = .00000000001;
+
+		ProteinQuery[] queries = new ProteinQuery[2];
+		queries[0] = new ProteinQuery("548663");
+		queries[1] = new ProteinQuery("514692457");
+
+		Gene[] genes = new Gene[1];
+		Gene gene = new Gene("Rad51", queries);
+
+		genes[0] = gene;
+
+		Genome[] genomes = new Genome[1];
+
+		//Gasterosteus aculeatus	69293	Animals	Fishes	GCA_000180675.1 	Contig	-	ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/180/675/GCA_000180675.1_ASM18067v1
+		Genome genome = new Genome("GCA_000180675.1", "Animal", "Fishes", "Contig", "69293", genes);
+
+		genomes[0] = genome;
+
+		Job newJob = new Job(jobName, evalue, genomes);
+
+		MakeRequest request = new MakeRequest();
+
+		request.sendRequest(newJob);
+
+	}
+}
